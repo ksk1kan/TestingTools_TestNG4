43f66712c2908e329bc155508b0dabd0a62a2b62
ksk1kan <birkanfiliz_35.5@hotmail.com>
2022-11-14 12:27:31 +0300

    XML File, Basic, MultipleClass, MultipleTest yapıldı.

---
+++ src/Gun03/_02_Intro.java
@@ -0,0 +1,12 @@
+package Gun03;
+
+import org.testng.annotations.Test;
+
+public class _02_Intro {
+
+    @Test
+    void Test1() { System.out.println("*** Intro_02_ --> Test 1 ***"); }
+
+    @Test
+    void Test2() { System.out.println("*** Intro_02_ --> Test 2 ***"); }
+}
